6738d737a42c837ea9d2ecbe133ec381d7ff7d43
Justin Marshall <justinmarshall20@gmail.com>
2025-02-28 08:18:16 -0800

    Compression now compiles.

--- Code/Libraries/Include/Lib/BaseType.h
+++ Code/Libraries/Include/Lib/BaseType.h
@@ -122,7 +122,8 @@ typedef unsigned int			UnsignedInt;	  	// 4 bytes
 typedef unsigned short		UnsignedShort;		// 2 bytes 
 typedef short							Short;					  // 2 bytes 
 typedef unsigned char			UnsignedByte;			// 1 byte		USED TO BE "Byte"
-typedef char							Byte;							// 1 byte		USED TO BE "SignedByte"
+// jmarshall - switch byte to unsigned char
+typedef unsigned char					Byte;							// 1 byte		USED TO BE "SignedByte"
 typedef char							Char;							// 1 byte of text
 typedef bool							Bool;							// 
 // note, the types below should use "long long", but MSVC doesn't support it yet
--- Code/Libraries/Source/Compression/CompressionManager.cpp
+++ Code/Libraries/Source/Compression/CompressionManager.cpp
@@ -262,7 +262,7 @@ Int CompressionManager::compressData( CompressionType compType, void *srcVoid, I
 		*(Int *)(dest+4) = 0;
 
 		unsigned long outLen = destLen;
-		Int err = z_compress2( dest+8, &outLen, src, srcLen, level );
+		Int err = compress2( (Bytef *)(dest+8), &outLen, (const Bytef *)src, srcLen, level ); // jmarshall
 
 		if (err == Z_OK || err == Z_STREAM_END)
 		{
@@ -333,7 +333,7 @@ Int CompressionManager::decompressData( void *srcVoid, Int srcLen, void *destVoi
 #endif
 
 		unsigned long outLen = destLen;
-		Int err = z_uncompress(dest, &outLen, src+8, srcLen-8);
+		Int err = uncompress((Bytef *)dest, &outLen, (const Bytef *)src+8, srcLen-8); // jmarshall
 		if (err == Z_OK || err == Z_STREAM_END)
 		{
 			return outLen;
